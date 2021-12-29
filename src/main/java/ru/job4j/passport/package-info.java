e37ad7cc31c630fced4a29dd5a14aa616c15043f
CyberfuzZ <keepthelight@icloud.com>
2021-12-29 16:49:46 +0300

    save

---
+++ src/main/java/ru/job4j/passport/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Класс package-info
+ *
+ * ! /save, сохранить данные паспорта
+ * todo: /update?id=*, обновить данные паспорта
+ * todo: /delete?id=*, удалить данные паспорта
+ * ! /find, загрузить все паспорта
+ * todo: /find?seria=*, загрузить паспорта с заданной серией
+ * todo: /unavaliabe, загрузить паспорта чей срок вышел
+ * todo: /find-replaceable, загрузить паспорта, которые нужно заменить в ближайшие 3 месяца
+ *
+ * @author Evgeniy Zaytsev
+ * @version 1.0
+ */
+package ru.job4j.passport;
