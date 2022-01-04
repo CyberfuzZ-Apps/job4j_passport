b3c93f27e4db916eba2cb7f6b1021cc171692ddf
CyberfuzZ-Apps <cyberfuzzapps@gmail.com>
2022-01-04 19:46:07 +0300

    2. Асинхронное взаимодействие [#458497]

---
+++ src/main/java/ru/job4j/passport/config/KafkaProducerConfig.java
@@ -0,0 +1,50 @@
+package ru.job4j.passport.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.ListSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import ru.job4j.passport.model.Passport;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Класс KafkaProducerConfig
+ *
+ * @author Evgeniy Zaytsev
+ * @version 1.0
+ */
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+
+    @Bean
+    public KafkaTemplate<Integer, List<Passport>> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<Integer, List<Passport>> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+}
---
+++ src/main/java/ru/job4j/passport/controller/KafkaPassportController.java
@@ -0,0 +1,39 @@
+package ru.job4j.passport.controller;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.job4j.passport.model.Passport;
+import ru.job4j.passport.service.PassportService;
+
+import java.util.List;
+
+/**
+ * Класс KafkaPassportController
+ *
+ * @author Evgeniy Zaytsev
+ * @version 1.0
+ */
+@Component
+@EnableScheduling
+public class KafkaPassportController {
+
+    private final KafkaTemplate<Integer, List<Passport>> template;
+    private final PassportService passportService;
+
+    public KafkaPassportController(KafkaTemplate<Integer, List<Passport>> template,
+                                   PassportService passportService) {
+        this.template = template;
+        this.passportService = passportService;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sendUnavailablePassports() {
+        List<Passport> unavailablePassports = passportService.findUnavailablePassports();
+        if (!unavailablePassports.isEmpty()) {
+            template.send("unavailable", unavailablePassports);
+        }
+    }
+}
